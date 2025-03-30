7fcb309d774be7c0b4e52758c725cf4864b30d81
andreifiyas <andrewfiyas@inbox.ru>
2025-03-30 20:27:33 +0300

    Fix 1 ex

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,6 +1,7 @@
 function factorial(n) {
+  if (n === 0) return 1;
   let finalResult = 1;
-  for (let i = 1; i < n; i--) {
+  for (let i = 1; i <= n; i++) {
     finalResult *= i;
   }
   return finalResult;
