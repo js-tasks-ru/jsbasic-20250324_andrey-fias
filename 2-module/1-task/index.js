6363f94052432c85fcd23a622215f4a6d078e6d8
andreifiyas <andrewfiyas@inbox.ru>
2025-04-03 13:56:08 +0300

    Third homework

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    let value = salaries[key];
+    if (typeof value === 'number' && isFinite(value)) {
+      sum += value;
+    }
+  }
+
+  return sum;
 }
