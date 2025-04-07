9091f04e18ba8e029d6a0c445e7519c5449dae99
andreifiyas <andrewfiyas@inbox.ru>
2025-04-07 18:29:18 +0300

    Fourth homework

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let parts = str.split('-');
+  for (let i = 1; i < parts.length; i++) {
+    parts[i] = parts[i][0].toUpperCase() + parts[i].slice(1);
+  }
+  return parts.join('');
 }
