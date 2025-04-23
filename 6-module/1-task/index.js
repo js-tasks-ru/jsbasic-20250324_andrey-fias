257d61e681a274a6fd0fd4bcf154b8e62507be83
andreifiyas <andrewfiyas@inbox.ru>
2025-04-23 18:16:13 +0300

    Six homework + part of personal project

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,36 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `;
+    const tbody = this.elem.querySelector('tbody');
+    for (let row of rows) {
+      const tr = document.createElement('tr');
+
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;
+      tr.querySelector('button').addEventListener('click', () => {
+        tr.remove();
+      });
+
+      tbody.appendChild(tr);
+    }
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,74 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.currentSlide = 0;
+    this.elem = this.render();
+    this.initCarousel();
+    this.addEventListeners();
+  }
+
+  render() {
+    const slideItems = this.slides.map(slide => `
+      <div class="carousel__slide" data-id="${slide.id}">
+        <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `).join('');
+
+    const carouselTemplate = `
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left" style="display: none">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner">
+          ${slideItems}
+        </div>
+      </div>
+    `;
+
+    return createElement(carouselTemplate);
+  }
+
+  initCarousel() {
+    this.inner = this.elem.querySelector('.carousel__inner');
+    this.arrowRight = this.elem.querySelector('.carousel__arrow_right');
+    this.arrowLeft = this.elem.querySelector('.carousel__arrow_left');
+  }
+
+  addEventListeners() {
+    this.arrowRight.addEventListener('click', () => this.moveSlide(1));
+    this.arrowLeft.addEventListener('click', () => this.moveSlide(-1));
+    this.elem.addEventListener('click', (event) => {
+      const button = event.target.closest('.carousel__button');
+      if (button) {
+        const slide = event.target.closest('.carousel__slide');
+        const productId = slide.dataset.id;
+        this.elem.dispatchEvent(
+          new CustomEvent('product-add', {
+            detail: productId,
+            bubbles: true,
+          })
+        );
+      }
+    });
+  }
+
+  moveSlide(direction) {
+    const slideWidth = this.inner.offsetWidth;
+    const maxIndex = this.slides.length - 1;
+    this.currentSlide += direction;
+    this.inner.style.transform = `translateX(-${this.currentSlide * slideWidth}px)`;
+    this.arrowLeft.style.display = this.currentSlide === 0 ? 'none' : '';
+    this.arrowRight.style.display = this.currentSlide === maxIndex ? 'none' : '';
   }
 }
+
